814ad85c026ea23629e49ad8cb879718a9cbcdee
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-24 22:52:35 +0600

    Math class

---
+++ JavaMathClass.java
@@ -0,0 +1,15 @@
+package MathClass;
+
+public class JavaMathClass {
+	public static void main(String[] args) {
+		System.out.println("----Use of Java MATH class----\n\n");
+		
+		// Link of all Math class
+		//https://www.javatpoint.com/java-math
+		
+		int x = 20;
+		int y = 10;
+		
+		System.out.printf("Maximun Number in %d and %d is %d",x,y,Math.max(x, y));
+	}
+}
